6e01c91386332850fcb5ec47ee9fd4dff83e3823
Gustavo de Leon <gdeleontobias@gmail.com>
2018-02-09 17:55:35 -0600

    pruebas bubble

--- Lab3_Sorts/src/lab3_sorts/Main.java
+++ Lab3_Sorts/src/lab3_sorts/Main.java
@@ -17,41 +17,23 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        // TODO code application logic here
         Sort bubble = new Sort();
         
-        try {
-            String sCadena;
-            System.out.println("Bienvenido");
-            //Funcion para abrir y leer el documento 
-            FileReader fr = new FileReader("datosRandom.txt");
-            BufferedReader bf = new BufferedReader(fr);
-            //bucle para hacer las funciones dependiendo de las lineas del documento
-            while ((sCadena = bf.readLine())!=null) {       
-                sCadena =bf.readLine();
-                StringTokenizer st = new StringTokenizer(sCadena," "); 
-                int dimension = st.countTokens(); 
-                int []arrNumeros = new int[dimension]; 
-                int i=0;
-                
-                
-                while(st.hasMoreTokens()) { 
-                    arrNumeros[i] = Integer.parseInt(st.nextToken());   
-                i++; 
-                }
-                    
-            } 
-            //bubble.bubbleSort(arrNumeros);
-                    //for(int item=0; item<arrNumeros.length;item++ ){
-                    //System.out.println(arrNumeros[i]);
-                    //}
- 
-        //Sirve si algun dato del documento no sirva
-        }catch(Exception e){
-            System.out.println("Documento no encontrado o dañado :( por favor verificar");
-            }
-        System.out.println("Gracias por esperar");
-      }
-  
+        
+        bubble.llenar("random.txt");
+        
+        System.out.println("Bienvenido");
+        
+        System.out.println("Array Before Bubble Sort");  
+        for(int item=0; item < bubble.datos.length; item++){  
+                System.out.print(bubble.datos[item] + " ");  
+        }  
+        
+        System.out.println();  
+        
+        bubble.bubbleSort(bubble.datos);//sorting array elements using bubble sort
+        
+        System.out.println("Array After Bubble Sort");  
+        
+    }  
 }
--- Lab3_Sorts/src/lab3_sorts/Sort.java
+++ Lab3_Sorts/src/lab3_sorts/Sort.java
@@ -5,40 +5,64 @@
  */
 package lab3_sorts;
 
+import java.io.*;
+import java.util.*;
+
 /**
  *
  * @author Gustavo
  */
 public class Sort {
-    private int[] arrays;
+     public Comparable[] datos=new Comparable[3000];
     
-   
-    public int[] getArrays() {
-        return arrays;
+     public Comparable[] getDatos() {
+        return datos;
     }
 
-    public void setArrays(int[] arrays) {
-        this.arrays = arrays;
+    public void setDatos(Comparable[] datos) {
+        this.datos = datos;
     }
-    
-            
-    public void bubbleSort(int[] arr) {
-      boolean cambios = false;
-      int aux;
-      while (true) {
-            cambios = false;
-            
-            for (int i = 0; i < arr.length; i++) {                                       
-                  if (arr[i] < arr[i - 1]) {                          
-                        aux = arr[i];
-                        arr[i] = arr[i - 1];
-                        arr[i - 1] = aux;
-                        cambios = true;
-                  }
-            }   
-            if(cambios=false)
-                break;
-      }
-      //return arr;
+ 
+    public void llenar(String texto) {
+        File f;
+        FileWriter w;
+        BufferedWriter bw;
+        PrintWriter wr;
+
+        try{
+        f= new File(texto);
+        w = new FileWriter(f);
+        bw = new BufferedWriter(w);
+        wr = new PrintWriter(bw);
+        Random aleatorio = new Random();
+        
+        for(int i=0; i<2999; i++){
+            datos[i] = aleatorio.nextInt(3001);
+            int x = (int) datos[i];
+            wr.println(String.valueOf(x));
+        }
+        wr.close();
+        bw.close();
+        }catch(IOException e){
+            System.out.println("Hubo un error");
+        }
+    } 
+
+    public void bubbleSort(Comparable[]datos) {  
+        int n = datos.length;  
+        int temp = 0;  
+         for(int i=0; i < n; i++){  
+            for(int j=1; j < (n-i); j++){  
+                if(datos[j-1].compareTo(datos[j]) <0 ){  
+                    //swap elements  
+                    temp = (int) datos[j-1];  
+                    datos[j-1] = datos[j];  
+                    datos[j] = temp;  
+                }               
+            }  
+        }
+        for(int item=0; item < datos.length; item++){  
+                System.out.println(datos[item] + " ");  
+        }
     }
 }
