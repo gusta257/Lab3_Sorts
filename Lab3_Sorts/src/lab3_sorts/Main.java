ca4adc687571df735264edcad2d6117325252f6f
LCEST <lcesturban10@gmail.com.com>
2018-02-08 18:47:27 -0600

    leer archivo

--- Lab3_Sorts/src/lab3_sorts/Main.java
+++ Lab3_Sorts/src/lab3_sorts/Main.java
@@ -3,19 +3,35 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package lab3_sorts;
-
+import java.io.*;
 /**
  *
- * @author Gustavo
+ * @author Gustavo De Leon 17085
+ * @author Luis Esturban 17256
  */
 public class Main {
 
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         // TODO code application logic here
+        System.out.println("Bienvenido");
+        try {
+            String sCadena;
+            //Funcion para abrir y leer el documento 
+            FileReader fr = new FileReader("datosRandom.txt");
+            BufferedReader bf = new BufferedReader(fr);
+            //bucle para hacer las funciones dependiendo de las lineas del documento
+            while ((sCadena = bf.readLine())!=null) {       
+                System.out.println(sCadena);
+            } 
+        //Sirve si algun dato del documento no sirva
+        }catch(Exception e){
+            System.out.println("Documento no encontrado o dañado :( por favor verificar");
+            }
+        System.out.println("Gracias por esperar");
     }
-    
 }
