1db2f9de4eda84261ac01e78a88cd2631bfa8c21
LCEST <lcesturban10@gmail.com.com>
2018-02-09 19:24:22 -0600

    Merge

--- Lab3_Sorts/src/Main.java
+++ Lab3_Sorts/src/Main.java
@@ -11,24 +11,33 @@ public class Main {
      */
     
     public static void main(String[] args) {
-        Sort bubble = new Sort();
+        Sort sorts = new Sort();
         
         
-        bubble.llenar("random.txt");
+        sorts.llenar("random.txt");
         
         System.out.println("Bienvenido");
         
         System.out.println("Array Before Bubble Sort");  
-        for(int item=0; item < bubble.datos.length; item++){  
-                System.out.print(bubble.datos[item] + " ");  
+        for(int item=0; item < sorts.datos.length; item++){  
+                System.out.print(sorts.datos[item] + " ");  
         }  
         System.out.println();         
-        bubble.bubbleSort(bubble.datos);//sorting array elements using bubble sort
+        sorts.bubbleSort(sorts.datos);//sorting array elements using bubble sort
         
         System.out.println("Array After Bubble Sort");  
-                for(int i=0; i < bubble.datos.length; i++){  
-                        System.out.print(bubble.datos[i] + " ");  
-                }    
+                for(int i=0; i < sorts.datos.length; i++){  
+                        System.out.print(sorts.datos[i] + " ");  
+                }  
+                //Merge
+        System.out.println();         
+        sorts.mergeSort(sorts.datos);//sorting array elements using bubble sort
+        
+        System.out.println("Array After merge Sort");  
+                for(int i=0; i < sorts.datos.length; i++){  
+                        System.out.print(sorts.datos[i] + " ");  
+                }
+        
         
         
     }  
--- Lab3_Sorts/src/Sort.java
+++ Lab3_Sorts/src/Sort.java
@@ -61,4 +61,78 @@ public class Sort {
    
         }
     }
+    public int[] mergeSort(int[] input){
+		
+		if(input.length == 1){
+			return input;
+		}
+		
+		int middle = (int) Math.ceil((double)input.length / 2);
+		int[] left = new int[middle];
+		
+		int rightLength = 0;
+		if(input.length % 2 == 0){
+			rightLength = middle;
+		}
+		else{
+			rightLength = middle - 1;
+		}
+		int[] right = new int[rightLength];
+		
+		int leftIndex = 0; 
+		int rightIndex = 0;
+		
+		for (int i = 0; i < input.length; i++) {
+			if(i < middle){
+				left[leftIndex] = input[i];
+				leftIndex++;
+			}
+			else{
+				right[rightIndex] = input[i];
+				rightIndex++;
+			}
+		}
+		
+		left = mergeSort(left);
+		right = mergeSort(right);
+		
+		return merge(left, right);
+	}
+	
+	
+	private int[] merge(int[] left, int[] right){
+		int[] result = new int[left.length + right.length];
+		int leftIndex = 0;
+		int rightIndex = 0;
+		int resultIndex = 0;
+		
+		while(leftIndex < left.length || rightIndex < right.length){
+			if(leftIndex < left.length && rightIndex < right.length){
+				if(left[leftIndex] < right[rightIndex]){
+					result[resultIndex] = left[leftIndex];
+					leftIndex++;
+					resultIndex++;
+				}
+				else{
+					result[resultIndex] = right[rightIndex];
+					rightIndex++;
+					resultIndex++;
+				}
+			}
+			else if(leftIndex < left.length){
+				for (int i = resultIndex; i < result.length; i++) {
+					result[i] = left[leftIndex];
+					leftIndex++;
+				}
+			}
+			else if(rightIndex < right.length){
+				for (int i = resultIndex; i < result.length; i++) {
+					result[i] = right[rightIndex];
+					rightIndex++;
+				}
+			}
+		}
+		
+		return result;
+	}
 }
